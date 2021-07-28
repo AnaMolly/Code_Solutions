f652009a060d4aaf5016a1bf8717824c0665049d
AnaMolly <analia.mohamdally@gmail.com>
2021-07-27 21:52:52 -0400

    set up server. set up react portion. started models, type defs, resolvers. installed dependencies.

---
+++ server/models/Profile.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const profileSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  }
+});
+
+
+const Profile = model('Profile', profileSchema);
+
+module.exports = Profile;
