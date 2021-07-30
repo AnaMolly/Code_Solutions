d4bfbf369f56dd0491b55a0e4203c7855da624e8
Kevin Crawford <crawfordkevin101@gmail.com>
2021-07-30 10:56:39 -0400

    applied minor changes to navbar

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,44 +1,49 @@
 import React from 'react';
-import '../Footer/Footer.css'
+import '../Footer/Footer.css';
+
 const Footer = () => {
     return(
         <div className='main-footer'>
             <div className='container'>
                 <div className='row'>
-                {/* Column1 */}
-                <div className='col1'></div>
-                    <h4>Code Solutions</h4>
-                    <ul className='list-unstyled'>
-                        <li>123-456-7890</li>
-                        <li>Toronto, Canada</li>
-                        <li> 6 Queen's Park Cres W</li>
-                    </ul>                        
-                {/* Column2 */}
-                <div className='col2'></div>
-                    <h4>Code Solutions</h4>
-                    <ul className='list-unstyled'>
-                        <li>123-456-7890</li>
-                        <li>Toronto, Canada</li>
-                        <li> 6 Queen's Park Cres W</li>
-                    </ul>
-                {/* Column3 */}
-                <div className='col3'></div>
-                    <h4>Code Solutions</h4>
-                    <ul className='list-unstyled'>
-                        <li>123-456-7890</li>
-                        <li>Toronto, Canada</li>
-                        <li> 6 Queen's Park Cres W</li>
-                    </ul>
+                    {/* Column1 */}
+                    {/* <div className='col1'>
+                        <h4>Code Solutions</h4>
+                        <ul className='list-unstyled'>
+                            <li>123-456-7890</li>
+                            <li>Toronto, Canada</li>
+                            <li> 6 Queen's Park Cres W</li>
+                        </ul>
+                    </div> */}
+                                                
+                    {/* Column2 */}
+                    {/* <div className='col2'>
+                        <h4>Code Solutions</h4>
+                        <ul className='list-unstyled'>
+                            <li>123-456-7890</li>
+                            <li>Toronto, Canada</li>
+                            <li> 6 Queen's Park Cres W</li>
+                        </ul>
+                    </div> */}
+                        
+                    {/* Column3 */}
+                    {/* <div className='col3'>
+                        <h4>Code Solutions</h4>
+                        <ul className='list-unstyled'>
+                            <li>123-456-7890</li>
+                            <li>Toronto, Canada</li>
+                            <li> 6 Queen's Park Cres W</li>
+                        </ul>
+                    </div> */}
                 </div>
-            </div>
             <hr/>
             <div className="row">
                 <p className='col-sm'>
                     &copy;{new Date().getFullYear()} K.A.D.D. Development | All rights reserved | Terms of Service | Privacy
                 </p>
-
             </div>
         </div>
+    </div>
     )
 }
 
