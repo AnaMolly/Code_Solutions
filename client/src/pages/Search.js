0e8932576acb610a3642c4daa49dd24a594d14e5
AnaMolly <analia.mohamdally@gmail.com>
2021-08-02 16:06:49 -0400

    adding changes to search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,15 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {render} from 'react-dom'
 import { useQuery } from '@apollo/client';
 import {QUERY_USERS } from '../utils/queries';
-import DeveloperList from '../components/DeveloperList';
+import DeveloperList from '../components/DeveloperList'
+import FEDeveloperList from '../components/FrontEndList';
 
 export default function SearchDev() {
 
-    
+    const [searchResults, setSearchResults] = useState('')
     const { loading, data } = useQuery(QUERY_USERS);
     console.log(data)
     const users = data?.users || [];
-    const developers = users.filter(user => user.primaryFocus === "Front-End")
+    const fedevelopers = users.filter(user => user.primaryFocus === "Front-End")
+    const bedevelopers = users.filter(user => user.primaryFocus === "Back-End")
+    const fsdevelopers = users.filter(user => user.primaryFocus === "Full-Stack")
+
+   
+    
+    const HandleSearch = (e) => {
+    
+        const { target } = e;
+        const inputType = target.name;
+       
+        console.log(inputType)
+
+    //     if (inputType==='frontend'){
+    //         return(
+    //                     <div>
+    //                     {
+    //                         <DeveloperList
+    //                         fedevelopers={fedevelopers}
+    //                         />
+    //                     }
+    //                     </div> 
+    //         )
+    //     } else if (inputType==='backend'){
+    //         return(
+
+    //             <div>{
+    //                 <DeveloperList
+    //                 bedevelopers={bedevelopers}
+    //                 />
+    //             }
+    //             </div>
+            
+        
+    // )
+    //     } else {
+    //         return(
+                
+    //                     <div>
+    //                         <h1 className="mainh1" style={{color:'white'}}>Search Results:</h1>
+    //                         {loading ? (
+    //                         <div>Loading...</div>
+    //                     ) : (
+    //                         <DeveloperList
+    //                         fsdevelopers={fsdevelopers}
+    //                         />
+    //                     )}
+    //                     </div>
+                
+                
+    //         )
+    //     }
+
+    }
+    
+   
 
     return(
         <div>
@@ -17,23 +74,24 @@ export default function SearchDev() {
                 <div style={{ backgroundColor:"#4AB8B1", margin:'40px 40px 0px 40px', border:'thin solid #4AB8B1', borderRadius:'3rem'}}>
                     <h1 className="mainh1" style={{padding:'60px 45px 45px 45px',color:'white'}}> What type of developer are you searching for?</h1>
                     <div className='searchbuttons'>
-                        <a name='frontend' className="search-but">  Front-end</a>
-                        <a name='backend'className="search-but"> Back-end</a>
-                        <a name='fullstack' className="search-but">  Full-stack</a>
+                        <a name='frontend' className="search-but" onClick={HandleSearch}>  Front-end</a>
+                        <a name='backend'className="search-but" onClick={HandleSearch}> Back-end</a>
+                        <a name='fullstack' className="search-but" onClick={HandleSearch}>  Full-stack</a>
                     </div>
                 </div>
                 <hr style={{opacity:'0.5', margin:'35px 160px 35px 160px', color:'lightgray'}}/>
                 <div>
-                    <h1 className="mainh1" style={{color:'white'}}>Search Results:</h1>
-                    {loading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <DeveloperList
-                    developers={developers}
-                    />
-                )}
-                </div>
-                </div>
+                            <h1 className="mainh1" style={{color:'white'}}>Search Results:</h1>
+                            {loading ? (
+                            <div>Loading...</div>
+                        ) : (
+                            <FEDeveloperList
+                            fedevelopers={fedevelopers}
+                            />
+                        )}
+                        </div>
+                
+            </div>
         </div>
         
         
