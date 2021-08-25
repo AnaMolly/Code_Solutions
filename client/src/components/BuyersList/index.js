5c1a8fa772b9a8fb3e1f5bdc1bb41f1562d614bf
AnaMolly <analia.mohamdally@gmail.com>
2021-08-25 14:45:04 -0400

    added company to buyer card

--- client/src/components/BuyersList/index.js
+++ client/src/components/BuyersList/index.js
@@ -12,7 +12,8 @@ const HomeDevList = ({ buyers }) => {
               <Card.Body style={{ textAlign:'center', padding:'30px 20px 40px 20px'}}>
                   <Card.Title  style={{ color:'black', padding:'10px', fontSize:'30px', fontWeight:'200'}}>{buyer.fullName}</Card.Title>
                   <Card.Img variant="top" src={buyer.profileImage} style={{maxWidth:'284px'}}/>
-                  <Card.Text  style={{ color:'black', padding:'20px 15px 10px 15px', fontSize:'20px'}}> <span style={{color:'gray'}}>Looking for: </span>{buyer.userDescription}</Card.Text>
+                  <Card.Text  style={{ color:'black', padding:'20px 15px 10px 15px', fontSize:'20px'}}> <span style={{color:'gray'}}>Company: </span>{buyer.company}</Card.Text>
+                  <Card.Text  style={{ color:'black', padding:'20px 15px 10px 15px', fontSize:'20px'}}> <span style={{color:'gray'}}>Looking for: </span>{buyer.servicesOffered}</Card.Text>
                   <a className="infobut" variant="primary" style={{ margin: '10px'}} href={`/profiledev/${buyer._id}`}>
                   View profile
                   </a>
